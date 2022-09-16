6d934ca93e5d2dbbf782fa11a549e7102f47ba74
emmanuelcortes2 <109642972+emmanuelcortes2@users.noreply.github.com>
2022-09-16 10:01:13 -0500

    Add dynamic content in JS

--- app.js
+++ app.js
@@ -1,109 +1,113 @@
-// let require;
-// const { toggle } = require();{
+const btn = document.querySelector('#menu');
 
-// function dropFunction() {
-//    document.getElementById('drop-content').classList.toggle('show');
-// }
-// dropFunction();
+function opnMenu() {
+  document.querySelector('.drop-content').style.display = 'block';
+}
 
-// toggle.addEventListener('click', () => {
-//   toggle.classList.dropFunction();
-// })
-// }
+btn.addEventListener('click', opnMenu);
 
-const workSection = document.getElementById("portfolio");
+const closeBtn = document.querySelector('.cls-btn');
+
+function clsMenu() {
+  document.querySelector('.drop-content').style.display = 'none';
+}
+
+closeBtn.addEventListener('click', clsMenu);
+
+const closeBtn1 = document.querySelector('.drop-content');
+
+closeBtn1.addEventListener('click', clsMenu);
+
+const workSection = document.getElementById('portfolio');
 
 const htmlArray = [
   {
-    card_name: "card1",
-    card_inner_name: "card11",
-    mwork_img: "./images/Works1.png",
-    dwork_img: "./images/desk1.svg",
-    tonic_div: "Tonic",
-    tonic_h1: "Tonic",
-    tonic_h10: "Tonic",
-    canopy_div: "canopy-cont",
-    canopy0_h2: "CANOPY",
-    canopyd_h2: "CANOPY",
-    work_list: "Back End Dev", 
-    work_list2: "2015",
-    tonic_h2: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-    tonic_h21: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-    buttons_ul: "buttons",
-    button_html: "html",
-    button_css: "css",
-    button_javascript: "javaScript",
-    see_button: "See Project",
-    
+    card_name: 'card1',
+    card_inner_name: 'card11',
+    mwork_img: './images/Works1.png',
+    dwork_img: './images/desk1.svg',
+    tonic_div: 'Tonic',
+    tonic_h1: 'Tonic',
+    tonic_h10: 'Tonic',
+    canopy_div: 'canopy-cont',
+    canopy0_h2: 'CANOPY',
+    canopyd_h2: 'CANOPY',
+    work_list: 'Back End Dev',
+    work_list2: '2015',
+    tonic_h2: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    tonic_h21: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    buttons_ul: 'buttons',
+    tonic_h22: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    seeButton: '1',
+    live_version: 'https://Goruchie.github.io/Goruchie.github.io/',
+    source: 'https://github.com/Goruchie/Goruchie.github.io',
 
   },
   {
-    card_name: "card2",
-    card_inner_name: "card22",
-    mwork_img: "./images/Works2.png",
-    dwork_img: "./images/desk2.svg",
-    tonic_div: "Tonic2",
-    tonic_h1: "Multi-Post Stories",
-    tonic_h10: "Multi-Post Stories",
-    canopy_div: "canopy-cont",
-    canopy0_h2: "CANOPY",
-    canopyd_h2: "FACEBOOK",
-    work_list: "Back End Dev", 
-    work_list2: "2015",
-    tonic_h2: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-    tonic_h21: "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.",
-    buttons_ul: "buttons",
-    button_html: "html",
-    button_css: "css",
-    button_javascript: "javaScript",
-    see_button: "See Project",
+    card_name: 'card2',
+    card_inner_name: 'card22',
+    mwork_img: './images/Works2.png',
+    dwork_img: './images/desk2.svg',
+    tonic_div: 'Tonic2',
+    tonic_h1: 'Multi-Post Stories',
+    tonic_h10: 'Multi-Post Stories',
+    canopy_div: 'canopy-cont',
+    canopy0_h2: 'CANOPY',
+    canopyd_h2: 'FACEBOOK',
+    work_list: 'Back End Dev',
+    work_list2: '2015',
+    tonic_h2: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    tonic_h21: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    buttons_ul: 'buttons',
+    tonic_h22: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    seeButton: '2',
+    live_version: 'https://Goruchie.github.io/Goruchie.github.io/',
+    source: 'https://github.com/Goruchie/Goruchie.github.io',
   },
   {
-    card_name: "card3",
-    card_inner_name: "card44",
-    mwork_img: "./images/Works3.png",
-    dwork_img: "./images/desk3.svg",
-    tonic_div: "Tonic",
-    tonic_h1: "Multi-Post Stories",
-    tonic_h10: "Facebook 360",
-    canopy_div: "canopy-cont",
-    canopy0_h2: "CANOPY",
-    canopyd_h2: "CANOPY",
-    work_list: "Back End Dev", 
-    work_list2: "2015",
-    tonic_h2: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    card_name: 'card3',
+    card_inner_name: 'card44',
+    mwork_img: './images/Works3.png',
+    dwork_img: './images/desk3.svg',
+    tonic_div: 'Tonic',
+    tonic_h1: 'Multi-Post Stories',
+    tonic_h10: 'Facebook 360',
+    canopy_div: 'canopy-cont',
+    canopy0_h2: 'CANOPY',
+    canopyd_h2: 'CANOPY',
+    work_list: 'Back End Dev',
+    work_list2: '2015',
+    tonic_h2: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     tonic_h21: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
-    buttons_ul: "buttons",
-    button_html: "html",
-    button_css: "css",
-    button_javascript: "javaScript",
-    see_button: "See Project",
+    buttons_ul: 'buttons',
+    tonic_h22: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    seeButton: '3',
+    live_version: 'https://Goruchie.github.io/Goruchie.github.io/',
+    source: 'https://github.com/Goruchie/Goruchie.github.io',
 
   },
   {
-    card_name: "card4",
-    card_inner_name: "card44",
-    mwork_img: "./images/Works4.png",
-    dwork_img: "./images/desk4.svg",
-    tonic_div: "Tonic2",
-    tonic_h1: "Multi-Post Stories",
-    tonic_h10: "Uber Navigation",
-    canopy_div: "canopy-cont",
-    canopy0_h2: "CANOPY",
-    canopyd_h2: "CANOPY",
-    work_list: "Back End Dev", 
-    work_list2: "2015",
-    tonic_h2: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-    tonic_h21: "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.",
-    buttons_ul: "buttons",
-    button_html: "html",
-    button_css: "css",
-    button_javascript: "javaScript",
-    see_button: "See Project",
-  }
-]
-
-// let html_inside = ""
+    card_name: 'card4',
+    card_inner_name: 'card44',
+    mwork_img: './images/Works4.png',
+    dwork_img: './images/desk4.svg',
+    tonic_div: 'Tonic2',
+    tonic_h1: 'Multi-Post Stories',
+    tonic_h10: 'Uber Navigation',
+    canopy_div: 'canopy-cont',
+    canopy0_h2: 'CANOPY',
+    canopyd_h2: 'CANOPY',
+    work_list: 'Back End Dev',
+    work_list2: '2015',
+    tonic_h2: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    tonic_h21: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    buttons_ul: 'buttons',
+    tonic_h22: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    seeButton: '4',
+    live_version: 'https://Goruchie.github.io/Goruchie.github.io/',
+    source: 'https://github.com/Goruchie/Goruchie.github.io',
+  },
+];
 
 htmlArray.forEach((e) => {
   workSection.innerHTML += `
@@ -125,15 +129,74 @@ htmlArray.forEach((e) => {
               <h2 class="tonic-des0">${e.tonic_h2}</h2>
               <h2 class="tonic-des01">${e.tonic_h21}</h2>
                 <ul class="${e.buttons_ul}">
-                  <li class="html-button">${e.button_html}</li>
-                  <li class="css-button">${e.button_css}</li>
-                  <li class="js-button">${e.button_javascript}</li>
-                </ul>
-                  <button class="see-project-sign">${e.see_button}</button>   
+                  <li class="html-button">html</li>
+                  <li class="css-button">css</li>
+                  <li class="js-button">javaScript</li>
+                </ul>                
+                  <button id="${e.seeButton}" onclick=clickPopup(this.id) class="see-project-sign">See Project</button>
         </div>
     </div>
+  </div>`;
+});
+
+const popupWindow = document.getElementById('popupWindow');
+
+/* eslint-disable no-unused-vars */
+
+const clickPopup = (seeButton) => {
+  const modals = htmlArray.filter((e) => e.seeButton === seeButton);
+  popupWindow.style.display = 'block';
+
+  popupWindow.innerHTML = `
+    <div class="popup-div">  
+      <div class="popup-cont">  
+      <h2 class="popup-title">${modals[0].tonic_h1}</h2>
+        <img onclick="clsPopup()" src="./images/Icon2.png" alt="" class="popupClose closePop">
+          <div class="pop-canopy-cont">
+            <h2 class="canopy0">${modals[0].canopy0_h2}</h2>
+              <h2 class="canopyd">${modals[0].canopyd_h2}</h2>
+                <ul class="canopy-li-pop">
+                  <li>${modals[0].work_list}</li>
+                  <li>${modals[0].work_list2}</li>
+                </ul>
+          </div>
+            <img class="mwork" src="${modals[0].mwork_img}" alt="Snapshoot Portfolio-1">
+            <img class="dwork1" src="./images/uf.png" alt="Snapshoot Portfolio-1">
+              <h2 class="tonic-des0-pop">${modals[0].tonic_h22}</h2>
+              <div class="textcont">
+                <h2 class="tonic-des01-pop">${modals[0].tonic_h22}</h2>
+                <div class="button-container">
+                  <ul class="buttons-pop">
+                    <li class="html-button">html</li>
+                    <li class="css-button">css</li>
+                    <li class="js-button">javaScript</li>
+                  </ul>
+                    <img class="vector4" src="./images/Vector 4.png" alt="vector">
+                    <div class="boxes-cont">
+                      <div class="live-box">
+                        <div class="just-base">
+                        <a href="${modals[0].live_version}" target="_blank" class="see-live">See live
+                        </a>
+                          <img class="live-img" src="./images/live.png" alt="live-icon">                          
+                      </div>
+                      </div>
+                        <div class="source-box">
+                        <div class="just-base1">
+                          <a href="${modals[0].source}" target="_blank" class="see-source">See Source
+                            </a>
+                              <img class="source-img" src="./images/source.png" alt="source-icon">                          
+                        </div>
+                        </div>
+                    </div>  
+      </div>
   </div>
+  </div>
+  </div>
+    `;
+};
 
-  `
-})
+/* eslint-disable no-unused-vars */
 
+const clsPopup = () => {
+  popupWindow.style.display = 'none';
+};
