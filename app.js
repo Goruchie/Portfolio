c66a74742f380f249d7fe2d8549651ef654e50dd
Marcos Gualtero Lourenzo <marcosgualtero43253@gmail.com>
2022-09-16 12:23:45 -0300

    add modal styles & .js proper data

--- app.js
+++ app.js
@@ -27,16 +27,16 @@ const htmlArray = [
     mwork_img: './images/Works1.png',
     dwork_img: './images/desk1.svg',
     tonic_div: 'Tonic',
-    tonic_h1: 'Tonic',
+    name: 'Tonic',
     tonic_h10: 'Tonic',
     canopy_div: 'canopy-cont',
     canopy0_h2: 'CANOPY',
     canopyd_h2: 'CANOPY',
     work_list: 'Back End Dev',
     work_list2: '2015',
-    tonic_h2: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     tonic_h21: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    buttons_ul: 'buttons',
+    technologies: 'buttons',
     tonic_h22: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
     seeButton: '1',
     live_version: 'https://Goruchie.github.io/Goruchie.github.io/',
@@ -49,16 +49,16 @@ const htmlArray = [
     mwork_img: './images/Works2.png',
     dwork_img: './images/desk2.svg',
     tonic_div: 'Tonic2',
-    tonic_h1: 'Multi-Post Stories',
+    name: 'Multi-Post Stories',
     tonic_h10: 'Multi-Post Stories',
     canopy_div: 'canopy-cont',
     canopy0_h2: 'CANOPY',
     canopyd_h2: 'FACEBOOK',
     work_list: 'Back End Dev',
     work_list2: '2015',
-    tonic_h2: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     tonic_h21: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
-    buttons_ul: 'buttons',
+    technologies: 'buttons',
     tonic_h22: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
     seeButton: '2',
     live_version: 'https://Goruchie.github.io/Goruchie.github.io/',
@@ -70,16 +70,16 @@ const htmlArray = [
     mwork_img: './images/Works3.png',
     dwork_img: './images/desk3.svg',
     tonic_div: 'Tonic',
-    tonic_h1: 'Multi-Post Stories',
+    name: 'Multi-Post Stories',
     tonic_h10: 'Facebook 360',
     canopy_div: 'canopy-cont',
     canopy0_h2: 'CANOPY',
     canopyd_h2: 'CANOPY',
     work_list: 'Back End Dev',
     work_list2: '2015',
-    tonic_h2: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     tonic_h21: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
-    buttons_ul: 'buttons',
+    technologies: 'buttons',
     tonic_h22: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
     seeButton: '3',
     live_version: 'https://Goruchie.github.io/Goruchie.github.io/',
@@ -92,16 +92,16 @@ const htmlArray = [
     mwork_img: './images/Works4.png',
     dwork_img: './images/desk4.svg',
     tonic_div: 'Tonic2',
-    tonic_h1: 'Multi-Post Stories',
+    name: 'Multi-Post Stories',
     tonic_h10: 'Uber Navigation',
     canopy_div: 'canopy-cont',
     canopy0_h2: 'CANOPY',
     canopyd_h2: 'CANOPY',
     work_list: 'Back End Dev',
     work_list2: '2015',
-    tonic_h2: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     tonic_h21: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
-    buttons_ul: 'buttons',
+    technologies: 'buttons',
     tonic_h22: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
     seeButton: '4',
     live_version: 'https://Goruchie.github.io/Goruchie.github.io/',
@@ -116,7 +116,7 @@ htmlArray.forEach((e) => {
       <img class="mwork" src="${e.mwork_img}" alt="Snapshoot Portfolio-1">
       <img class="dwork" src="${e.dwork_img}" alt="Snapshoot Portfolio-1">
         <div class="${e.tonic_div}">
-          <h1 class="Multi-Post-Stories2">${e.tonic_h1}</h1>
+          <h1 class="Multi-Post-Stories2">${e.name}</h1>
           <h1 class="Multi-Post-Stories1">${e.tonic_h10}</h1>
             <div class="${e.canopy_div}">
               <h2 class="canopy0">${e.canopy0_h2}</h2>
@@ -126,9 +126,9 @@ htmlArray.forEach((e) => {
                     <li>${e.work_list2}</li>
                   </ul>
             </div>
-              <h2 class="tonic-des0">${e.tonic_h2}</h2>
+              <h2 class="tonic-des0">${e.description}</h2>
               <h2 class="tonic-des01">${e.tonic_h21}</h2>
-                <ul class="${e.buttons_ul}">
+                <ul class="${e.technologies}">
                   <li class="html-button">html</li>
                   <li class="css-button">css</li>
                   <li class="js-button">javaScript</li>
@@ -150,7 +150,7 @@ const clickPopup = (seeButton) => {
   popupWindow.innerHTML = `
     <div class="popup-div">  
       <div class="popup-cont">  
-      <h2 class="popup-title">${modals[0].tonic_h1}</h2>
+      <h2 class="popup-title">${modals[0].name}</h2>
         <img onclick="clsPopup()" src="./images/Icon2.png" alt="" class="popupClose closePop">
           <div class="pop-canopy-cont">
             <h2 class="canopy0">${modals[0].canopy0_h2}</h2>
